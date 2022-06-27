5ec893b8a8c8825f7cea9545ba135c9b18a0075d
Matronator <5470780+matronator@users.noreply.github.com>
2022-06-28 01:40:25 +0200

    update facades.neon

--- README.md
+++ README.md
@@ -8,6 +8,22 @@ Automatically generate Entity and related files from the console.
 composer require matronator/generator --dev
 ```
 
+#### Troubleshooting
+
+If you get this error when trying to install:
+
+```
+matronator/generator dev-master requires composer-runtime-api ^2.2 -> found composer-runtime-api[2.1.0] but it does not match the constraint.
+```
+
+Run this command to update composer to the latest version:
+
+```
+composer self-update
+```
+
+If you can't or don't want to update composer, use version `"^1.0"` of this package as that doesn't depend on Composer runtime API 2.2.
+
 #### Additional steps (temporary - will be automated in the future)
 
 1. Copy `mtrgen` file from `project-root/vendor/matronator/generator/bin/` to `project-root/bin/`
--- src/Generator/FileGenerator.php
+++ src/Generator/FileGenerator.php
@@ -5,14 +5,26 @@ declare(strict_types=1);
 namespace Matronator\Generator;
 
 use Nette\PhpGenerator\PsrPrinter;
+use Nette\Neon\Neon;
 
 class FileGenerator
 {
+    public const FACADES_CONFIG = 'app/config/app/facades.neon';
+
     public static function writeFile(FileObject ...$files) {
         $printer = new PsrPrinter;
 
         foreach ($files as $file) {
             file_put_contents($file->directory . $file->filename, $printer->printFile($file->contents));
+
+            if (stripos($file->filename, 'Facade') !== false) {
+                $neon = Neon::decodeFile(self::FACADES_CONFIG);
+                $neon['services'][lcfirst($file->filename)] = [
+                    'class' => 'App\Model\Database\Facade\\' . $file->filename,
+                    'inject' => true,
+                ];
+                file_put_contents('nette.safe://'.self::FACADES_CONFIG, Neon::encode($neon, Neon::BLOCK));
+            }
         }
     }
 }
